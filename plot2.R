cad1ef2972146f15f0a6622caeedf4da029f293e
victorhenry-stack <victorhenry@google.com>
2026-04-09 10:56:27 -0300

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,13 +1,13 @@
-# 1. READ AND SUBSET (Same as Plot 1)
-data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
-subset_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+# 1. READ AND SUBSET (Same as Plot 1) 
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?") cleans the data so we can use it
+subset_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,] #Subsets the data to use only the data range we need.
 
-# 2. THE "STITCH": Combine Date and Time
+# 2. Combine Date and Time
 # We create a new column that looks like "2007-02-01 00:01:00"
 datetime <- strptime(paste(subset_data$Date, subset_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 subset_data$Datetime <- as.POSIXct(datetime)
 
-# 3. OPEN THE PNG DEVICE
+# 3. OPEN THE PNG DEVICE #we need to open it to be able to plot something
 png("plot2.png", width=480, height=480)
 
 # 4. CREATE THE PLOT
