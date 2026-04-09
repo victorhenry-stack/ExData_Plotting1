3e83c6fb6fe3b3cbfb57f911589af8a3c7458d49
victorhenry-stack <victorhenry@google.com>
2026-04-09 11:06:47 -0300

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,24 +1,24 @@
-# 1. DATA LOADING & SUBSETTING (Same as before)
-data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+# 1. READ AND SUBSET (Same as Plot 1,2 and 3) 
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?")
 subset_data <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-# 2. DATETIME CONVERSION
+# 2. Combine Date and Time (Same as plot 2 and 3)
 datetime <- strptime(paste(subset_data$Date, subset_data$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 subset_data$Datetime <- as.POSIXct(datetime)
 
-# 3. OPEN PNG DEVICE
+# 3. OPEN PNG DEVICE (same as all others)
 png("plot4.png", width=480, height=480)
 
 # 4. SET UP THE 2x2 GRID
-# c(2, 2) means 2 rows and 2 columns
-par(mfrow = c(2, 2)) 
+par(mfrow = c(2, 2)) # c(2, 2) means 2 rows and 2 columns We need this since we are plotting 4 charts in the same image
+
 
 # --- PLOT 1 (Top Left): Global Active Power ---
-plot(subset_data$Global_active_power ~ subset_data$Datetime, type="l", 
+plot(subset_data$Global_active_power ~ subset_data$Datetime, type="l", #plot Global_active_power as a function of datatime (x-axis)
      ylab="Global Active Power", xlab="")
 
 # --- PLOT 2 (Top Right): Voltage ---
-plot(subset_data$Voltage ~ subset_data$Datetime, type="l", 
+plot(subset_data$Voltage ~ subset_data$Datetime, type="l", #plot voltage as a function of time
      ylab="Voltage", xlab="datetime")
 
 # --- PLOT 3 (Bottom Left): Energy Sub-metering ---
